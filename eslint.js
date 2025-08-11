e8ff17c98ce4c2e209f341f5af8cb525e752fd62
govarthanan-k <57951675+govarthanan-k@users.noreply.github.com>
2025-08-12 01:55:02 +0530

    Update eslint.js

--- eslint.js
+++ eslint.js
@@ -1,23 +1,33 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      react,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      react.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
-    plugins: {
-      react,
+    rules: {
+      'react/require-default-props': [
+        'warn',
+        {
+          ignoreFunctionalComponents: false,
+          forbidDefaultForRequired: true,
+        },
+      ],
     },
     languageOptions: {
       ecmaVersion: 2020,
@@ -33,8 +43,5 @@ export default tseslint.config([
         version: 'detect',
       },
     },
-    rules: {
-      'react/require-default-props': 'error',
-    },
   },
-])
+]);
